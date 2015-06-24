a84285dc5b16fd5c5e46c73f5a559932aab0c810
arsenal4wang <wangyguang@qq.com>
2015-06-24 14:40:43 +0800

    idea

--- my/org/wang/a_tcp/User.java
+++ my/org/wang/a_tcp/User.java
@@ -6,22 +6,31 @@ import java.lang.reflect.Proxy;
 public class User {
 	int id;
 	String name;
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	public User(int id,String name){
-		this.id=id;
-		this.name=name;
+
+	public User(int id, String name) {
+		this.id = id;
+		this.name = name;
 	}
+
 	public int getId() {
 		return id;
 	}
-	
+
 	public void setId(int id) {
 		this.id = id;
+		System.out.print("PPP");
 	}
+/*
+	这是一个测试页
+ */
+
 }
 
